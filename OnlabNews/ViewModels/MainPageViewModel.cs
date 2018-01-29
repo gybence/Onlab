2aa39ff87c05d6cbc6a6f6cc2194caa33ca685af
gyongyossy.bence <gyongyossy0122@gmail.com>
2018-01-29 16:46:19 +0100

    Set up Prism

---
+++ OnlabNews/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Windows.Navigation;
+using System.Collections.ObjectModel;
+
+namespace OnlabNews.ViewModels
+{
+	public class MainPageViewModel : ViewModelBase
+	{
+		public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+		{
+			base.OnNavigatedTo(e, viewModelState);
+
+		}
+	}
+}
