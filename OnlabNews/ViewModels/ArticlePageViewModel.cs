58c3b91ccbea453adab69c0bf3db6771628ade65
Bence <gyongyossy0122@gmail.com>
2018-10-14 01:54:34 +0200

    RSS list imrovements, Scrapy webserver integration init

--- OnlabNews/ViewModels/ArticlePageViewModel.cs
+++ OnlabNews/ViewModels/ArticlePageViewModel.cs
@@ -1,20 +1,18 @@
-﻿using Prism.Windows.Mvvm;
+﻿using OnlabNews.Models;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
 using Prism.Windows.Navigation;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using OnlabNews.Views;
-using OnlabNews.Models;
-using Prism.Commands;
-using Windows.System;
-using Windows.Foundation.Collections;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Core;
 
 namespace OnlabNews.ViewModels
 {
-	public class ArticlePageViewModel: ViewModelBase
+	public class ArticlePageViewModel : ViewModelBase
 	{
 		#region properties
 
@@ -22,13 +20,15 @@ namespace OnlabNews.ViewModels
 
 		DataTransferManager _dataTransferManager;
 
+		private CoreDispatcher dispatcher;
+
 		public DelegateCommand ShareButtonCommand { get; private set; }
 
 		private ArticleItem _article;
 		public ArticleItem Article
 		{
 			get
-			{																	//bing >_<
+			{                                                                   //bing >_<
 				return _article ?? new ArticleItem { Title = "No Title", Uri = "https://bing.com" };
 			}
 			set { SetProperty(ref _article, value); }
@@ -44,6 +44,7 @@ namespace OnlabNews.ViewModels
 
 			_dataTransferManager = DataTransferManager.GetForCurrentView();
 			_dataTransferManager.DataRequested += DataRequestedForSharing;
+			dispatcher = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
 		}
 
 		private void DataRequestedForSharing(DataTransferManager sender, DataRequestedEventArgs args)
@@ -54,14 +55,50 @@ namespace OnlabNews.ViewModels
 		}
 
 		private void ShareButtonClick()
-		{		
+		{
 			DataTransferManager.ShowShareUI();
 		}
 
-		public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+		private async Task RequestArticleScrapeAsync(String toScrape)
+		{
+			using (HttpClient httpClient = new HttpClient())
+			{
+				httpClient.BaseAddress = new Uri(@"http://localhost:8080");
+				httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+				httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("utf-8"));
+
+				string endpoint = @"";
+
+				try
+				{
+					//JsonConvert.SerializeObject(yourPocoHere), Encoding.UTF8, "application/json"
+					HttpContent content = new StringContent("{\"url\":\""+ toScrape + "\"}");
+					HttpResponseMessage response = await httpClient.PostAsync(endpoint, content);
+
+					if (response.IsSuccessStatusCode)
+					{
+						string jsonResponse = await response.Content.ReadAsStringAsync();
+						//do something with json response here
+					}
+				}
+				catch (Exception)
+				{
+					//Could not connect to server
+					//Use more specific exception handling, this is just an example
+				}
+			}
+		}
+
+
+		public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
 		{
 			Article = e.Parameter as ArticleItem;
 			base.OnNavigatedTo(e, viewModelState);
+			await dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+			{
+				 RequestArticleScrapeAsync("file:///C:/Users/Bence/Desktop/quotes.html");
+			});
+			bool asd = false;
 		}
 	}
 }
